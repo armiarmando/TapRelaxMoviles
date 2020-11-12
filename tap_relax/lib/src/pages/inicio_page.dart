d522682bfacce33103d7512fdf78fe2444b8844d
Armando <gutierrezmartinezjosermando@gmail.com>
2020-11-11 23:25:17 -0600

    Se agrego los puntos de acerca con el mensaje de alarma

--- tap_relax/lib/src/pages/inicio_page.dart
+++ tap_relax/lib/src/pages/inicio_page.dart
@@ -3,7 +3,26 @@ import 'package:flutter/material.dart';
 class PaginaInicial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: _pageView());
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Tap Relax'),
+          actions: [
+            PopupMenuButton(
+              itemBuilder: (BuildContext context) {
+                return <PopupMenuEntry<String>>[
+                  PopupMenuItem<String>(
+                      child: Text("Acerca de"), value: "acercade")
+                ];
+              },
+              onSelected: (String value) {
+                if (value == "acercade") {
+                  _mostrarAlerta(context);
+                }
+              },
+            )
+          ],
+        ),
+        body: _pageView());
   }
 
   Widget _pageView() {
@@ -19,6 +38,22 @@ class PaginaInicial extends StatelessWidget {
       ),
     );
   }
+}
+
+void _mostrarAlerta(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        content: AcercaDe(),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.check), onPressed: () => Navigator.pop(context))
+        ],
+        elevation: 24.0,
+      );
+    },
+  );
 }
 
 class Paginas extends StatelessWidget {
@@ -46,6 +81,45 @@ class Paginas extends StatelessWidget {
       onDoubleTap: () {
         Navigator.of(context).pushNamed(pag);
       },
+    );
+  }
+}
+
+class AcercaDe extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Tap Relax",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Padding(
+          child: Text(
+            "Versión 1.0",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 10),
+          ),
+          padding: EdgeInsets.all(16.0),
+        ),
+        Text(
+          "Programación y diseño",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 15),
+        ),
+        Text(
+          "Erick Moreno",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          "Armando Gutiérrez",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+      ],
+      mainAxisSize: MainAxisSize.min,
     );
   }
 }
